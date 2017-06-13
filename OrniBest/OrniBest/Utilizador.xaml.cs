791c805d043e809fbd1b42dadae2778e51e23729
TIago Henrique <tiagohenrique191@hotmail.com>
2017-06-13 17:42:25 +0100

    enviado

--- OrniBest/OrniBest/Utilizador.xaml.cs
+++ OrniBest/OrniBest/Utilizador.xaml.cs
@@ -113,7 +113,7 @@ namespace OrniBest
         private void bt_salvar_Click(object sender, RoutedEventArgs e)
         {
             int clube = cb_clube.SelectedIndex;
-            utilizador2 registo = new utilizador2(tb_nome.Text, System.Convert.ToInt32(tb_telemovel.Text),System.Convert.ToInt32(tb_STAM.Text),data_nascimento.Text,tb_morada.Text, tb_codigopostal.Text, clube);
+            utilizador2 registo = new utilizador2(tb_nome.Text, System.Convert.ToInt32(tb_telemovel.Text),tb_STAM.Text,data_nascimento.Text,tb_morada.Text, tb_codigopostal.Text, clube);
             utilizador2.AddRegistos(registo);
         }
     }
--- OrniBest/OrniBest/utilizador2.cs
+++ OrniBest/OrniBest/utilizador2.cs
@@ -22,13 +22,13 @@ namespace OrniBest
         private static List<utilizador2> util = new List<utilizador2>();
         public string nome;
         public long telemovel;
-        public long stam;
+        public string stam;
         public string data_nascimento;
         public string morada;
         public string codigo_postal;
         public long clube;
 
-        public utilizador2(string nome2, long telemovel2, long stam2, string data_nascimento2, string morada2, string codigo_postal2, long clube2)
+        public utilizador2(string nome2, long telemovel2, string stam2, string data_nascimento2, string morada2, string codigo_postal2, long clube2)
         {
             this.nome = nome2;
             this.telemovel = telemovel2;
@@ -53,7 +53,7 @@ namespace OrniBest
             while (reader.Read())
             {
                 utilizador2 newUtilizador = new utilizador2((string)reader["nome"], (long)reader["telemovel"],
-                                                (long)reader["STAM"], (string)reader["data_nascimento"],
+                                                (string)reader["STAM"], (string)reader["data_nascimento"],
                                                 (string)reader["Morada"], (string)reader["cod_postal"],
                                                 (long)reader["id_clube"]);
                 util.Add(newUtilizador);
@@ -68,7 +68,7 @@ namespace OrniBest
             {
                 SQLiteConnection myConn = new SQLiteConnection("Data Source=OrniFile_v1.db; version=3");
                 myConn.Open();
-                string sql_add = "UPDATE `utilizador` SET `nome`= '" + util.nome + "',`morada`= '" + util.morada + "',`cod_postal`= '" + util.codigo_postal + "',`telemovel`= " + util.telemovel + ",`STAM`= " + util.stam + ",`id_clube`= " + util.clube + ",`data_nascimento`= '" + util.data_nascimento + "' WHERE `id_utilizador` = 1";
+                string sql_add = "UPDATE `utilizador` SET `nome`= '" + util.nome + "',`morada`= '" + util.morada + "',`cod_postal`= '" + util.codigo_postal + "',`telemovel`= " + util.telemovel + ",`STAM`= '" + util.stam + "',`id_clube`= " + util.clube + ",`data_nascimento`= '" + util.data_nascimento + "' WHERE `id_utilizador` = 1";
 
                 //"VALUES ('" + util.nome + "','" + util.telemovel + "','" + util.stam + "', '" + util.data_nascimento + "','" + util.morada + "')" + "','" + util.codigo_postal + "')" + "','" + util.clube + "')";
                 SQLiteCommand newCommand = new SQLiteCommand(sql_add, myConn);
